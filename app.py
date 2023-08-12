ecff0f18bd60bb4f2151d7e4750cfebaf6ad1623
mpraveen21 <127092738+mpraveen21@users.noreply.github.com>
2023-08-12 13:43:06 +0000

    Added HTML ans CSS

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask 
+from flask import Flask ,render_template
 
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "<h1><pre>PRAVEEN</pre></h1>"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
